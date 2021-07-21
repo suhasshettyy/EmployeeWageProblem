43d1ffce38dacec5db1da5affba579e1b4576776
suhasshettyy <scshetty07@gmail.com>
2021-07-21 10:08:15 +0530

    [Suhas] Add: EmployeeWageProblem

---
+++ Employeeproblem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProblem
+{
+    class Employeeproblem
+    {
+        public void Introduction()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employeeproblem Employee = new Employeeproblem();
+            Employee.Introduction();
+        }
+    }
+}
