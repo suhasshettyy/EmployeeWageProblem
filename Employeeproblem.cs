3418ec4e45cea64540e7de4213600791963f6729
suhasshettyy <scshetty07@gmail.com>
2021-07-21 10:37:30 +0530

    [Suhas] Add: UC2-empWage

--- Employeeproblem.cs
+++ Employeeproblem.cs
@@ -6,14 +6,20 @@ namespace EmployeeWageProblem
 {
     class Employeeproblem
     {
-        public void Attendance()
+        public void Wage()
         {
             Random random = new Random();
+            int wageprhr = 20;
+            int hr = 8;
+            int x = wageprhr * hr;
             int empcheck = random.Next(0, 2);
-            if (empcheck == 1)
-                Console.WriteLine("Employee is Present");
+            if (empcheck == 0)
+                Console.WriteLine("Employee is absent");
             else
-                Console.WriteLine("Employee is Absent");
+            {
+                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee Wage is:" + x.ToString());
+            }
         }
     }
 }
